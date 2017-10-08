a573148d948a59558d9def4ef200a82a31ab014e
Kailasa-aravinda <kailasa.aravind@gmail.com>
2017-10-08 16:51:38 +0530

    Initial commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -16,7 +16,7 @@ app.controller('chatController', function ($scope, $sce) {
 $scope.messages=[
     ];
 
-    var exampleSocket = new WebSocket('wss://swiftcodebeardyman.herokuapp.com/ deployed to Heroku');
+    var exampleSocket = new WebSocket('wss://swiftcodebeardyman.herokuapp.com/chatSocket');
     exampleSocket.onmessage =function  (event) {
         var jsonData = JSON.parse(event.data);
         jsonData.time = new Date()
