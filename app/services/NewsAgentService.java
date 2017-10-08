85e638b15fd0ae2256c54582deafdd978719e487
Kailasa-aravinda <kailasa.aravind@gmail.com>
2017-10-08 15:15:14 +0530

    news bot

--- app/services/NewsAgentService.java
+++ app/services/NewsAgentService.java
@@ -21,13 +21,13 @@ public class NewsAgentService {
                     .setQueryParameter("lang", "en")
                     .setQueryParameter("sessionId", sessionId.toString())
                     .setQueryParameter("timezone", "2017-08-09T03:25:23+0530")
-                    .setHeader("Authorization", "Bearer Bearer 946df4ead6524dbcaeb5c6c2409462b6")
-            .get();
+                    .setHeader("Authorization", "Bearer 946df4ead6524dbcaeb5c6c2409462b6").get();
             JsonNode response = responsePromise.thenApply(WSResponse::asJson).toCompletableFuture().get();
             newsAgentResponse.keyword = response.get("result").get("parameters").get("keyword").asText();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return newsAgentResponse;
-        }
+
     }
+}
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -4,19 +4,21 @@ app.controller('chatController', function ($scope, $sce) {
 $scope.messages=[
     ];
 
-    var  exampleSocket =  new  WebSocket("wss://swiftcode-ws-chat.herokuapp.com/chatSocket");
-        exampleSocket.onmessage  =   function  (event) {
-            var jsonData = JSON.parse(event.data);
-            jsonData.time = new Date()
-                .toLocaleTimeString();
-            $scope.messages.push(jsonData);
-            $scope.$apply();
-            console.log(jsonData);
-        };
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+    exampleSocket.onmessage =function  (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    }
+
+
         $scope.sendMessage = function () {
                 exampleSocket.send($scope.userMessage);
                 $scope.userMessage = "";
-            };
-            $scope.trust = $sce.trustAsHtml;
-            $scope.trust = $sce.trustAsHtml;
+        }
+
+        $scope.trust = $sce.trustAsHtml;
 });
